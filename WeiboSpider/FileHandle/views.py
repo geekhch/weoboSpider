3a542599316300a3a5c1ac3add98bf56f56a24cc
Bolvvv <605725874@qq.com>
2018-12-21 16:52:35 +0800

    finish api

--- WeiboSpider/FileHandle/views.py
+++ WeiboSpider/FileHandle/views.py
@@ -18,9 +18,27 @@ def upload_file(request):
         table = workbook.sheet_by_index(0)  # 取第一张工作簿
         print(table.cell(1, 1).value)
         functionName = table.cell(1, 1).value
-        if(functionName == 'UserIndexInfo'):
-            path = xls_gener.blogs_to_xls(table.cell(2, 1))
-            MAIL('微博博主主页内容', table.cell(3, 1), path)
+        if functionName == 'UserIndexInfo':
+            path = xls_gener.blogs_to_xls(int(table.cell(2, 1).value))
+            MAIL('博主所有微博内容', table.cell(3, 1), path)
+        elif functionName == 'UserBaseInfo':
+            uids = []
+            rows = table.row_values(2) # 获取第3行的所有值
+            for r in rows[1:]:
+                uids.append(int(r))
+            path = xls_gener.profile_to_xls(uids)
+            MAIL('根据uid生成用户基本信息', table.cell(3, 1), path)
+        elif functionName == 'UserWorldCloud':
+            path = xls_gener.blogs_to_xls(int(table.cell(2, 1).value))
+            MAIL('生成用户词云', table.cell(3, 1), path)
+        elif functionName == 'FansInfo':
+            path = xls_gener.blogs_to_xls(int(table.cell(2, 1).value))
+            MAIL('根据uid生成粉丝信息', table.cell(3, 1), path)
+        elif functionName == 'FansFollowingInfo':
+            path = xls_gener.blogs_to_xls(int(table.cell(2, 1).value))
+            MAIL('根据uid生成关注用户信息', table.cell(3, 1), path)
+        elif functionName == 'UserDetailInfo':
+            pass
         response = {
             'dataStatus': "1"
         }
--- WeiboSpider/run.py
+++ WeiboSpider/run.py
@@ -6,23 +6,28 @@ if __name__ == '__main__':
     xls_gener = DataView()
 
     # # 用户功能
-    # 1. 根据uid列表生成用户基本profile信息的excel文件
+    # 1. 生成一个用户所有微博到excel,返回excel路径
+    # 对应UserIndexInfo
+    path = xls_gener.blogs_to_xls(3554683503)
+
+    # 2. 根据uid列表生成用户基本profile信息的excel文件
+    # 对应UserBaseInfo
     uids = [6029786152, 5102089477, 2988799167,3554683503]
     path = xls_gener.profile_to_xls(uids)
-
-    # 2. 生成一个用户所有微博到excel,返回excel路径
-    path = xls_gener.blogs_to_xls(3554683503)
 
     # 3. 邮件发送附件
     MAIL('Weibo Spider','微博内容数据','709531006@qq.com', path)
 
     # 4. 生成词云，返回词云图片路径
+    # 对应UserWorldCloud
     path = xls_gener.word_cloud(3554683503)
 
     # 5. 根据uid生成粉丝基本信息的excel
+    # 对应FansInfo
     path = xls_gener.fans_profile_to_xls(3554683503)
 
     # 6. 根据uid生成关注用户基本信息的excel
+    # 对应FansFollowingInfo
     path = xls_gener.folows_profile_to_xls(3554683503)
 
     # 7. 微博情感组成分析，生成统计图
