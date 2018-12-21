51765c19482dd271428a86e21b2d8cce8195da0b
Bolvvv <605725874@qq.com>
2018-12-21 11:05:15 +0800

    fix

--- WeiboSpider/FileHandle/views.py
+++ WeiboSpider/FileHandle/views.py
@@ -1,18 +1,30 @@
 # -*- coding: utf-8 -*-
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import JsonResponse, HttpResponse
+from django.views.decorators.csrf import csrf_exempt
+from WeiboSpider.Spider.analysis import dataView
+from WeiboSpider.Spider.utils import *
 import xlrd
 
+xls_gener = dataView()
 
 # 接受前端传来的文件
+@csrf_exempt
 def upload_file(request):
     if request.method == "POST":
         print("post request")
         f = request.FILES['file']
         workbook = xlrd.open_workbook(filename=None, file_contents=f.read())
         table = workbook.sheet_by_index(0)  # 取第一张工作簿
-        print(table.cell(0,0).value)
-        return HttpResponse("hhh")
+        print(table.cell(1, 1).value)
+        functionName = table.cell(1, 1).value
+        if(functionName == 'UserIndexInfo'):
+            path = xls_gener.blogs_to_xls(table.cell(2, 1))
+            MAIL('微博博主主页内容', table.cell(3, 1), path)
+        response = {
+            'dataStatus': "1"
+        }
+        return JsonResponse(response)
     else:
         print("get request")
-        return render(request, 'index.html')
+        return HttpResponse("请使用POST方法")
--- WeiboSpider/Spider/run.py
+++ WeiboSpider/Spider/run.py
@@ -1,19 +1,20 @@
 from WeiboSpider.Spider.spider.spider import Spider
-from WeiboSpider.Spider.analysis import DataView
+from WeiboSpider.Spider.analysis import dataView
 from WeiboSpider.Spider.utils import *
 
 if __name__ == '__main__':
-    xls_gener = DataView()
+    xls_gener = dataView()
 
-    # 1. 爬取博主信息、微博发布和转发、关注博主id、被关注博主列表到数据库
+    # 1.功能名：微博博主博文内容
+    # 描述 生成一个用户所有微博到excel,返回excel文件
+    path = xls_gener.blogs_to_xls(6029786152)
+
+    # 2. 爬取博主信息、微博发布和转发、关注博主id、被关注博主列表到数据库
     sp = Spider(6029786152) # 获取yc的数据
 
-    # 2. 根据uid列表生成用户基本profile信息的excel文件
+    # 3. 根据uid列表生成用户基本profile信息的excel文件
     uids = [6029786152, 5102089477]
     path = xls_gener.profile_to_xls(uids)
-
-    # 3. 生成一个用户所有微博到excel,返回
-    path = xls_gener.blogs_to_xls(6029786152)
 
     # 4. 邮件发送附件
     MAIL('发送附件测试','605725874@qq.com', './assets/profile/profile_12-19-074734.xls')
